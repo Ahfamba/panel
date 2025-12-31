46146adcf36f386aff483d03fb0fb8adebbb57d0
Ahfamba <latifamakele219@gmail.com>
2025-12-31 04:00:29 +0300

    Add files via upload

---
+++ m933RsstkKXh.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/8065ba9ed330c6a1.js"
+  ],
+  "/_error": [
+    "static/chunks/dcdf2f69a69e96c1.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/api/react"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
